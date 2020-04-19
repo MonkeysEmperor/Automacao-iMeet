63630c55fb01af4f2e501c7b8427f1fe4704128b
Gabriel Amorim <gabriel.amorim@blink.com.br>
2020-04-19 15:43:16 -0300

    Updates

---
+++ precache-manifest.be2a299d166f2add5150d08b20f2a68b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1fb644718e1f87b16c4745aa1d5a6a9",
+    "url": "/Automacao-iMeet/index.html"
+  },
+  {
+    "revision": "87a1e3f9ec7d313ead01",
+    "url": "/Automacao-iMeet/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "f6c757f0b99c577d6c2a",
+    "url": "/Automacao-iMeet/static/css/main.6b9dcdb7.chunk.css"
+  },
+  {
+    "revision": "87a1e3f9ec7d313ead01",
+    "url": "/Automacao-iMeet/static/js/2.ee85883c.chunk.js"
+  },
+  {
+    "revision": "1bc4652f39d67028ed3fdd3981be1217",
+    "url": "/Automacao-iMeet/static/js/2.ee85883c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c757f0b99c577d6c2a",
+    "url": "/Automacao-iMeet/static/js/main.d5cab3c0.chunk.js"
+  },
+  {
+    "revision": "ee3ae0ced2e140d091a96d9554c57d13",
+    "url": "/Automacao-iMeet/static/js/main.d5cab3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70139ea3aace52481d70",
+    "url": "/Automacao-iMeet/static/js/runtime-main.d0e45818.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/Automacao-iMeet/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/Automacao-iMeet/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/Automacao-iMeet/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/Automacao-iMeet/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/Automacao-iMeet/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/automacao-imeet/precache-manifest.46e863c7084353faca0759b20bdd75ff.js"
+  "/Automacao-iMeet/precache-manifest.be2a299d166f2add5150d08b20f2a68b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/automacao-imeet/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Automacao-iMeet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
